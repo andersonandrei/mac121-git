4576de202e3fafe1166c4841b6facc86ac3cb023
Andrei <a.andreisilva@yahoo.com.br>
2016-08-21 08:29:33 -0300

    Nova implementação usando vetores mas não dinamicamente alocados.

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
-#define  MAX 255
+#define  MAX 999999
 
 int verifica (int n);
+int verifica (int n);
+void imprimeVetor(int prox[]);
+void initVetor(int prox[]);
 
 int main() {
-		int num,fn,interv,aux,cont = 0,impar=0;
+		int fn,interv,aux,seguinte,cont = 0,impar=0;
 		long int i,f;
+		int prox[MAX];
 		
 		scanf("%ld %ld",&i,&f);
 		interv = f - i;
 		fn = i;
 		
+		initVetor(prox);
+		
 		while(interv >= 0) {
 			aux = fn;
-			/* printf("\n Fazedno %d\n ",fn); */
-			while(aux != 1) {
-					impar=0;
-					printf("\n a");
+			printf("%d: ",aux);
+			while(aux > 1) {
 					if (aux%2 != 0) impar=1;
-					aux = verifica(aux);
-					printf("\n %ld",aux);
-					/*printf("%d\n", aux); */
-					if (impar==1) cont+=2;
-					else cont ++;
+					seguinte = verifica(aux);
+					if(prox[aux] != seguinte) {
+						prox[aux]=seguinte;
+					}
+					if (impar==1) {
+						cont+=2;
+					}
+					else {
+						cont++;
+					}
+					aux = seguinte;
+					impar = 0;
 			}
 			printf("%d \n",cont); 
 			fn++;
 			interv --;
 			cont = 0;
 		}
-		
 		return 0;
 }
-
 
 int verifica (int n){
 		if (n==1) return 1;
@@ -40,49 +49,13 @@ int verifica (int n){
 		else return (1.5*n)+0.5;
 }
 
-int procura (int bd[MAX][MAX], int num, int ultx, int ulty, int *pcont, int *pachou) {
-/* Função recebe a tabela banco de dados e o numero a ser procurado na 
- * tabela	e retorna a qnt de iterações pós encontrála ou 0 caso 
- * contrário */
-
-	int i,j,col = 0,n,ult=0,add=0,parar=0,ultl=0,contRec=0;
-	n=num;
-	i=0;
-	for (j=0; ultl == 0 && j < 12 ; j++) {
-			for (i=0; bd[i][j]!=1 && i < 12 ; i++) {
-				if ((bd[i][j] == n) && ((i!=ultx) && (j!=ulty))) {
-					(*pachou)++;
-					col = j;
-					for (col = j;bd[i][col] != 0; i ++) {
-						ult = bd[i][col];
-						if (ult != num)(*pcont)++;
-						ultx = i;
-						ulty = col;
-					}
-					if (ult != 1) {
-						add = procura (bd,ult,ultx,ulty,pcont,pachou);
-						contRec++;
-					}
-					else {
-						ultl=1;
-						return *pcont ;	
-					}
-				}
-			}
-	}
-	add-=contRec*add;
-	return *pcont+add;
-	
+void imprimeVetor(int prox[]) {
+	int i;
+	for(i=0;i<50;i++) printf("%d ",prox[i]);
 }
 
-void insere (int bd[MAX][MAX], int num) {
-	int i,j;
-	
-	for (i=0; i< MAX; i++){
-			for (j=0;j<MAX;j++) {
-				if (bd[i][j] != num) bd[i][j++] = num;				
-			}
-	}
-	
-	return;
+void initVetor(int prox[]) {
+	int i;
+	for (i=0;i<50;i++) prox[i] = -2;	
 }
+
