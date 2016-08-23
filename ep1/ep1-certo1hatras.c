4e4aa01e9052c99e634e48a36b88b77e9d5ebe88
Andrei <a.andreisilva@yahoo.com.br>
2016-08-23 00:14:31 -0300

    EP1 entregue

--- ep1/ep1-certo1hatras.c
+++ ep1/ep1-certo1hatras.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX 1000000
 
 unsigned long int collatz(int n);
 /* Recebe um inteiro n e verifica se o número recebido é ímpar ou par 
  * e então retorna o resultado de f(n) onde f é a função da 
  * conjectura de Collatz. */
  
-void initVetor (unsigned long int *prox);
-/*Recebe um ponteiro para um vetor de unsigned long int e irá inicializar as posições 
+void initVetor (unsigned long int prox[]);
+/*Recebe um vetor de unsigned long int e irá inicializar as posições 
  * de 2 até MAX do vetor com o valor -2. */
 
 int main() {
-	int intervalo, cont = 0, impar = 0, MAX = 3354432;
+	int intervalo, cont = 0, impar = 0;
 	unsigned long int fn, i, f, seguinte, aux;
-	unsigned long int *prox;
+	unsigned long int prox[MAX];
 	scanf("%lu %lu", &i, &f);
 	intervalo = f - i;
 	fn = i;
-	prox = malloc (MAX * sizeof(unsigned long int));
-	
-	if (prox == NULL) {
-		printf("\n Não foi possível alocar. Limite de memória excedida."
-		" Requesito mínimo: 256 mb de memória livre.");
-		free(prox);
-		return 0;
-	}
-	
 	initVetor(prox);
 	while (intervalo > 0) {
 		aux = fn;
 		while (aux > 1) {
 			if (aux >= MAX) {
-				printf("\nLimite de memória excedida, pois para a "
-				"última sequência \niniciada em: %lu chegamos "
-				"em: %lu que não é comportado pelo programa.", fn, aux);
+				printf("\nLimite de memória excedida, último " 
+				"número computado: %lu.", fn-1);
 				return 0;
 			}
 			if (aux % 2 != 0)
@@ -53,7 +43,6 @@ int main() {
 		intervalo--;
 		cont = 0;
 	}
-	free(prox);
 	return 0;
 }
 
@@ -63,8 +52,8 @@ unsigned long int collatz(int n){
 	else return (1.5 * n) + 0.5;
 }
 
-void initVetor(unsigned long int *prox) {
-	int i, MAX = 3354432;
+void initVetor(unsigned long int prox[]) {
+	int i;
 	prox[0] = prox[1] = 0;
 	for (i = 2; i < MAX; i++ ) prox[i] = 0;	
 }
